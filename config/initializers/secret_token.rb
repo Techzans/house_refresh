74d46c11281b40d8a1df7120e58d4ccdf93ee59b
Amy Mok <amymok@codeforamerica.org>
2014-03-19 18:04:18 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HouseRefresh::Application.config.secret_key_base = '218d9baa5aee409175aa5d24ad6c3bbcb62342f7aa68b2c386bd47ddaeacc8f61d038e87e98416d6684993e11405f5977de0ac33acdc53cbfeaeb8b32239113b'
